1f1123cfeea71482f5643b2d4c3fc20ba610a2a9
DamiannOrtega <pushofgod123@gmail.com>
2023-11-23 23:42:10 -0600

    Implementacion del archivo dat
    Ya quedó lo de solicitar al usuario los datos de la mercancia y se guardan en un .dat

--- main.cpp
+++ main.cpp
@@ -3,34 +3,68 @@
 #include <fstream>
 #include <iomanip> //Por si llegamos a usar setw
 
-struct Mercancia {
-    string nombre;
-    int volumen;
-    int costo;
-    int unidades;
-};
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
+const int MAX_MERCANCIAS = 100;
 
-int main(int argc, char const *argv[]){
-    fstream archivo;
+class Mercancia {
+    public:
+        string nombre;
+        int volumen;
+        int costo;
+        int unidades;
 
-    archivo.open("Buque.dat",ios::binary|ios::out|ios::app);
-    if(!archivo){
-        cout << "No se pudo abrir el archivo!" << endl;
-        cin.get();
-        return 1; //Terminar el programa
-    }
+    // Constructor
+    Mercancia() {}
+
+    // Constructor con parametros
+    Mercancia(const string& nombre, int volumen, int costo, int unidades)
+        : nombre(nombre), volumen(volumen), costo(costo), unidades(unidades) {}
+
+    // Otros metodos
+};
+
+
+//FUNCIONES
+
+//Función para validar si la cadena es de minúsculas
+bool validarMinus(const string& cadena);
+
+// Función para leer los datos desde la entrada del usuario
+void leerArchivo(Mercancia mercancias[], int& tiposMercancias, int& capacidadBuque);
+
+// Función para escribir los datos en el archivo "Buque.dat"
+void escribirArchivo(const string& nombreArchivo, const Mercancia mercancias[], int tiposMercancias, int capacidadBuque);
+
+//Función para imprimir el contenido del archivo (para verificar si se estan guardando los datos)
+void imprimirArchivo(const string& nombreArchivo);
+
+
+
+
+int main() {
+    Mercancia mercancias[MAX_MERCANCIAS];
+    int tiposMercancias, capacidadBuque;
+
+    //Leer datos desde la entrada del usuario
+    leerArchivo(mercancias, tiposMercancias, capacidadBuque);
+
+    //Escribir datos en el archivo "Buque.dat"
+    escribirArchivo("Buque.dat", mercancias, tiposMercancias, capacidadBuque);
+
+    imprimirArchivo("Buque.dat");
 
     // Mercancia mercancias[MAX_MERCANCIAS];
-    // int numTiposMercancias, capacidadBuque;
+    // int tiposMercancias, capacidadBuque;
 
     // Fase 1: Leer datos desde el archivo
-    //leerDatos("BUQUE.DAT", mercancias, numTiposMercancias, capacidadBuque);
+    //leerDatos("BUQUE.DAT", mercancias, tiposMercancias, capacidadBuque);
 
     // Fase 1: Mostrar información en pantalla
-    //imprimirDatos(mercancias, numTiposMercancias, capacidadBuque);
+    //imprimirDatos(mercancias, tiposMercancias, capacidadBuque);
 
     // Fase 2: Calculo de la forma más óptima (Implementar)
 
@@ -38,7 +72,99 @@ int main(int argc, char const *argv[]){
     // escribirResultados(solucion);
 
     // Fase 3: Formas posibles de embarcar la carga (Implementar)
-    // formasPosiblesDeEmbarcar(mercancias, numTiposMercancias, capacidadBuque);
+    // formasDeEmbarcar(mercancias, tiposMercancias, capacidadBuque);
 
     return 0;
+}
+
+
+//IMPLEMENTACION
+
+void escribirArchivo(const string& nombreArchivo, const Mercancia mercancias[], int tiposMercancias, int capacidadBuque) {
+    ofstream archivo(nombreArchivo);
+
+    if (!archivo) {
+        cerr << "Error al abrir el archivo " << nombreArchivo << endl;
+        exit(1);
+    }
+
+    archivo << tiposMercancias << endl;
+    archivo << capacidadBuque << endl;
+
+    for (int i = 0; i < tiposMercancias; ++i) {
+        archivo << mercancias[i].nombre << " " << mercancias[i].volumen << " " << mercancias[i].costo << " " << mercancias[i].unidades << endl;
+    }
+
+    archivo.close();
+}
+
+bool validarMinus(const string& cadena) {
+    for (char caracter : cadena) {
+        if (!islower(caracter)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Función para leer los datos desde la entrada del usuario
+void leerArchivo(Mercancia mercancias[], int& tiposMercancias, int& capacidadBuque) {
+    cout << "Ingrese la cantidad de tipos de mercancias (1-99): ";
+    cin >> tiposMercancias;
+
+    cout << "Ingrese la capacidad del buque (1-9999): ";
+    cin >> capacidadBuque;
+
+    //Ciclo for para solicitar al usuario las mercancias
+    for (int i = 0; i < tiposMercancias; ++i) {
+        string nombre;
+        int volumen, costo, unidades;
+
+        cout << "Ingrese los datos para la mercancia " << i + 1 << ": ";
+        cin >> nombre >> volumen >> costo >> unidades;
+
+        // Validaciones para las minusculas y el rango de cantidades 
+        if (nombre.length() > 20 || !validarMinus(nombre)) {
+            cerr << "Error: El nombre de la mercancia debe tener maximo 20 letras minusculas!" << endl;
+            exit(1);
+        }
+
+        if (volumen < 1 || volumen > 99999 || costo < 1 || costo > 99999 || unidades < 1 || unidades > 99999) {
+            cerr << "Error: Las cantidades deben ser enteros entre 1 y 99999" << endl;
+            exit(1);
+        }
+
+        mercancias[i] = Mercancia(nombre, volumen, costo, unidades);
+    }
+}
+
+void imprimirArchivo(const string& nombreArchivo) {
+    ifstream archivo(nombreArchivo);
+
+    if (!archivo) {
+        cerr << "Error al abrir el archivo " << nombreArchivo << endl;
+        exit(1);
+    }
+
+    int tiposMercancias, capacidadBuque;
+    archivo >> tiposMercancias >> capacidadBuque;
+
+
+    cout << endl << "Imprimiendo informacion............." << endl << endl;
+    cout << "Tipos de mercancias: " << tiposMercancias << endl;
+    cout << "Capacidad del buque: " << capacidadBuque << endl;
+
+    for (int i = 0; i < tiposMercancias; ++i) {
+        string nombre;
+        int volumen, costo, unidades;
+
+        archivo >> nombre >> volumen >> costo >> unidades;
+
+        cout << "Mercancia " << i + 1 << ": " << setw(15) << left << nombre
+             << "Volumen: " << setw(5) << volumen
+             << "Costo: " << setw(5) << costo
+             << "Unidades: " << setw(5) << unidades << endl;
+    }
+
+    archivo.close();
 }
